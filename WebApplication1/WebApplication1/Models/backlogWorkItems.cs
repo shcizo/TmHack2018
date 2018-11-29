63332c55ce899b7312dc40139eec95a86c01843f
Waqas Siddiqui <waqas.siddiqui@ntm.eu>
2018-11-29 16:46:44 +0100

    Get task title as well

---
+++ WebApplication1/WebApplication1/Models/backlogWorkItems.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class BacklogWorkItems
+    {
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+  }
+}
