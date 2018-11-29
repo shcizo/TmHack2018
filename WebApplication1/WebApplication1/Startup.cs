3bf59d7c4b975a35e1ca32ef81d0d03fc617329d
Samuel Enocsson <samuel.enocsson@teknomedia.se>
2018-11-29 14:34:14 +0100

    sdfg

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -24,6 +24,15 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+          services.AddCors(options =>
+          {
+            options.AddPolicy("AllowCors",
+              builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .AllowAnyHeader());
+          });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
